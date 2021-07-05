c5d97487e23d5cd5fe9c4086ddc54fc19fac3416
Felipe Borreli <felipeborreli@gmail.com>
2021-07-05 19:59:09 -0300

    test: testing husky

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -5,7 +5,7 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const sut = new RemoteAuthentication('url', httpPostClientSpy)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
