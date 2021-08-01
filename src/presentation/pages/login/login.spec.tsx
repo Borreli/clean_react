859eb87cd97c3cdf977ba850793511a6d286c10c
Felipe Borreli <felipeborreli@gmail.com>
2021-08-01 18:18:14 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,10 +5,21 @@ import Login from './login'
 describe('Login Component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
+
+    // Should not render spinner and error message
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
 
+    // Should start with submit button disabled
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+
+    // Should...
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.className).toContain('statusRefused')
+    const passwordStatus = getByTestId('email-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.className).toContain('statusRefused')
   })
 })
