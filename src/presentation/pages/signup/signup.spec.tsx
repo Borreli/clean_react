a2764485ac9a375d4fa58ceca36ff3139702a95f
Felipe Borreli <felipeborreli@gmail.com>
2021-08-30 01:15:03 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -39,7 +39,7 @@ describe('Signup Component', () => {
 
     // Should be email and password required inputs
     FormHelper.testStatusForField(sut, 'name', validationError)
-    FormHelper.testStatusForField(sut, 'email', 'Campo obrigatório')
+    FormHelper.testStatusForField(sut, 'email', validationError)
     FormHelper.testStatusForField(sut, 'password', 'Campo obrigatório')
     FormHelper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
   })
@@ -49,5 +49,12 @@ describe('Signup Component', () => {
     const { sut } = makeSut({ validationError })
     FormHelper.populateField(sut, 'name')
     FormHelper.testStatusForField(sut, 'name', validationError)
+  })
+
+  test('Should show email error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
+    FormHelper.populateField(sut, 'email')
+    FormHelper.testStatusForField(sut, 'email', validationError)
   })
 })
