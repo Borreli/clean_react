1a94f88b48b2971a2ae72cf5fff097495eb1c14a
Felipe Borreli <felipeborreli@gmail.com>
2021-09-03 05:08:33 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,21 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    // Input status Refused
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+
+    // Submit button disabled
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+
+    // Error-wrap empty
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
