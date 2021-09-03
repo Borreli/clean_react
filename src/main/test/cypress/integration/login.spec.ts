8bec6d60dbe39e48969c516a643ff902525f2015
Felipe Borreli <felipeborreli@gmail.com>
2021-09-03 05:17:15 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -5,9 +7,11 @@ describe('Login', () => {
 
   it('Should load with correct initial state', () => {
     // Input status Refused
+    cy.getByTestId('email').should('have.attr', 'readOnly')
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('password').should('have.attr', 'readOnly')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('have.css', 'background-color', 'rgb(255, 0, 0)')
@@ -16,6 +20,22 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
 
     // Error-wrap empty
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido!')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido!')
+      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
