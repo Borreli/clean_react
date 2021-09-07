8e5b96f802cf17eae854659110cfe4dcc1be4798
Felipe Borreli <felipeborreli@gmail.com>
2021-09-07 00:51:39 -0300

    refactor: improve input layout

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -10,14 +10,17 @@ describe('Login', () => {
 
   it('Should load with correct initial state', () => {
     // Input status Refused
-    cy.getByTestId('email').should('have.attr', 'readOnly')
-    cy.getByTestId('email-status')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('email')
       .should('have.attr', 'title', 'Campo obrigatório')
-      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
-    cy.getByTestId('password').should('have.attr', 'readOnly')
-    cy.getByTestId('password-status')
+      .should('have.attr', 'readOnly')
+    cy.getByTestId('email-label').should('have.attr', 'title', 'Campo obrigatório')
+
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('password')
       .should('have.attr', 'title', 'Campo obrigatório')
-      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+      .should('have.attr', 'readOnly')
+    cy.getByTestId('password-label').should('have.attr', 'title', 'Campo obrigatório')
 
     // Submit button disabled
     cy.getByTestId('submit').should('have.attr', 'disabled')
@@ -28,14 +31,14 @@ describe('Login', () => {
 
   it('Should present error state if form is invalid', () => {
     cy.getByTestId('email').focus().type(faker.random.word())
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'Valor inválido!')
-      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('email').should('have.attr', 'title', 'Valor inválido!')
+    cy.getByTestId('email-label').should('have.attr', 'title', 'Valor inválido!')
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'Valor inválido!')
-      .should('have.css', 'background-color', 'rgb(255, 0, 0)')
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('password').should('have.attr', 'title', 'Valor inválido!')
+    cy.getByTestId('password-label').should('have.attr', 'title', 'Valor inválido!')
 
     cy.getByTestId('submit').should('have.attr', 'disabled')
 
@@ -44,14 +47,14 @@ describe('Login', () => {
 
   it('Should present valid state if form is valid', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'Tudo certo!')
-      .should('have.css', 'background-color', 'rgb(0, 255, 0)')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'valid')
+    cy.getByTestId('email').should('not.have.attr', 'title')
+    cy.getByTestId('email-label').should('not.have.attr', 'title')
 
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'Tudo certo!')
-      .should('have.css', 'background-color', 'rgb(0, 255, 0)')
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'valid')
+    cy.getByTestId('password').should('not.have.attr', 'title')
+    cy.getByTestId('password-label').should('not.have.attr', 'title')
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
 
