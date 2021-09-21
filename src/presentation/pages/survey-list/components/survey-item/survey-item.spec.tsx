f67532b26d21e962451b54b2763d4192ee3f9ad2
Felipe Borreli <felipeborreli@gmail.com>
2021-09-21 04:05:57 -0300

    fix: remove console.log from tests

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -9,8 +9,6 @@ describe('SurveyItem Component', () => {
     const survey = mockSurveyModel()
     survey.didAnswer = true
     survey.date = new Date('2020-01-10T00:00:00')
-    console.log('date', survey.date)
-    console.log('month', survey.date.toLocaleString('pt-BR', { month: 'short' }))
     render(<SurveyItem survey={survey}/>)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
