085c098c16d4100a417557c763d44da93c643d2a
Felipe Borreli <felipeborreli@gmail.com>
2021-09-23 00:14:07 -0300

    feat: add SurveyList composition

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,5 +1,5 @@
 import { makeSignUpValidation } from '@/main/factories/pages/signup/signup-validation-factory'
-import { makeRemoteAddAccount } from '@/main/factories/usecases/add-account/remote-add-account-factory'
+import { makeRemoteAddAccount } from '@/main/factories/usecases'
 import { SignUp } from '@/presentation/pages'
 import React from 'react'
 
