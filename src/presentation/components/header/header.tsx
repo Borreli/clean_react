4fd434d4c51ca19d828bbd3e7f445261a414c476
Felipe Borreli <felipeborreli@gmail.com>
2021-09-28 00:11:26 -0300

    refactor: add useLogout to remove code duplication

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -3,14 +3,14 @@ import Styles from './header-styles.scss'
 import { useHistory } from 'react-router'
 import { Logo } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
+import { useLogout } from '@/presentation/hooks'
 
 const Header: React.FC = () => {
-  const history = useHistory()
-  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
-  const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+  const logout = useLogout()
+  const { getCurrentAccount } = useContext(ApiContext)
+  const buttonLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault()
-    setCurrentAccount(undefined)
-    history.replace('/login')
+    logout()
   }
   return (
     <header className={Styles.headerWrap}>
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
         <Logo />
         <div className={Styles.logoutWrap}>
           <span data-testid="username">{getCurrentAccount().name}</span>
-          <a data-testid="logout" href="#" onClick={logout}>Sair</a>
+          <a data-testid="logout" href="#" onClick={buttonLogout}>Sair</a>
         </div>
       </div>
     </header>
