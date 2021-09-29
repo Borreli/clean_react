3c1730b1b27a8376489ec2e195088effdedb7db5
Felipe Borreli <felipeborreli@gmail.com>
2021-09-29 17:56:51 -0300

    test: ensure SurveyList reload on button click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -20,6 +20,15 @@ describe('SurveyList', () => {
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde.')
   })
 
+  it('Should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente mais tarde.')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
+  })
+
   it('Should logout on AccessDeniedError', () => {
     mockAccessDeniedError()
     cy.visit('')
